fae7d2f9bb5a1d562501a41622c1e66f3fdc1a4f
Rohit <rohit.kisto@mail.utoronto.ca>
2019-12-02 11:30:54 -0500

    Selected data outs to csv

--- main.py
+++ main.py
@@ -3,30 +3,22 @@ import sys
 import csv
 
 # Constants
-MEASLES_DATA = 'measles.csv'
+MEASLES_DATA        = 'measles.csv'
 
-INCOME_LEVELS = {
-    1: 'WB_LI',
-    2: 'WB_LMI',
-    3: 'WB_UMI',
-    4: 'WB_UI'
-}
+INCOME_LEVELS       = {1: 'WB_LI', 2: 'WB_LMI', 3: 'WB_UMI', 4: 'WB_UI'}
 
-INDEX_COUNTRY = 0
-INDEX_INCOME_LEVEL = 1
+INDEX_COUNTRY       = 0
+INDEX_INCOME_LEVEL  = 1
 
 MIN_YEAR = 1980
 MAX_YEAR = 2017
 
 # Strings
-YEAR_PROMPT = 'Enter a year between 1980 & 2017: '
+YEAR_PROMPT     = 'Enter a year between 1980 & 2017: '
+INCOME_PROMPT   = '1 - Low Income\n2 - Lower Middle Income\n3 - Upper Middle Income\n4 - High Income\nEnter one of the above income levels: '
 
-INCOME_PROMPT = \
-'''1 - Low Income
-2 - Lower Middle Income
-3 - Upper Middle Income
-4 - High Income
-Enter one of the above income levels: '''
+COUNTRY         = 'Country'
+WB_IL           = 'World_Bank_Income_Level'
 
 def terminate(msg, error_code=1):
     print(msg)
@@ -46,22 +38,43 @@ if __name__ =='__main__':
         writer = csv.writer(output_file)
         reader = csv.reader(measles_file)
 
+        header = [COUNTRY, WB_IL]
+        
         # Get desired year from user
-        year = int(input(YEAR_PROMPT))
+        year = input(YEAR_PROMPT)
+
+        if year.lower() == 'all':
+            for i in range(2017, 1979, -1):
+                header.append(i)
+        else:
+            header.append(year)
 
         # Get desired income level from user
-        income_level = int(input(INCOME_PROMPT))
+        income_level = input(INCOME_PROMPT)
 
         # If the user input for income level is not a valid income level, terminate program
-        if not 0 < income_level < 5:
+        if income_level != 'all' and not 0 < int(income_level) < 5:
             terminate('That income level is not supported. Program will terminate.')
     
+        writer.writerow(header)
+
+        i = 0
         for row in reader:
-            if row[INDEX_INCOME_LEVEL] != INCOME_LEVELS[income_level]:
+            if i == 0 or (income_level != 'all' and row[INDEX_INCOME_LEVEL] != INCOME_LEVELS[income_level]):
+                i += 1
                 continue
             
-            print('Country: %s |%s: %s' % (row[INDEX_COUNTRY], int(year), row[2 + MAX_YEAR - int(year)]))
+            info = [row[INDEX_COUNTRY], row[INDEX_INCOME_LEVEL]]
 
+            if year == 'all':
+                for j in range(2, len(row)):
+                    info.append(row[j])
+            else:
+                info.append(row[2 + MAX_YEAR - int(year)])
 
+            writer.writerow(info)
+
+            i += 1
+    
     measles_file.close()
     
